d1fd3d13cf5e617e5dc037f3f4e59fdd82c430b7
hyaovi <hfyakre@gmail.com>
2018-04-06 23:43:53 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Genesis
+A boiler plate for static website developpement 
+
+## Getting started 
+ First you need [NodeJs](https://nodejs.org/en/download/) installed.
+ Clone this rep and run `npm install` or `yarn` to install the dependencies
+ Start the dev server by running `npm start` or `yarn start` 
+ 
+### Developement
+    Start the developpement server by running `npm start` or `yarn start` 
+
+### Production
+    Build a Production version of your website into 'prod' folder by running `npm build` or `yarn build` 
+    Start the production server by running `npm serveBuild` or `yarn serveBuild` 
+
+### Prerequisites
+You will need gulp install `npm -g gulp` or `yarn add gulp`
+
+### Dependencies
+Genesis depend on 
+[Browsersync](https://github.com/BrowserSync/browser-sync)
+[NodeJs](https://nodejs.org/en/download/)
+[Gulp](https://github.com/gulpjs/gulp)
+[Gulp-sass](https://github.com/dlmanning/gulp-sass)
+[Gulp-imagemin](https://github.com/sindresorhus/gulp-imagemin)
---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+const
+    browserSync = require('browser-sync').create(),
+    gulp =  require('gulp'),
+    sass = require('gulp-sass'),
+    imagemin = require('gulp-imagemin')
+;
+
+const path = {
+    src:{
+        html:'src/*.html',
+        scss:'src/scss/*.scss',
+        js:'src/js/*.js',
+        css:'src/css/',
+        img:'src/img/*',
+    },
+    build:{
+        html:'prod/',
+        js:'prod/js/',
+        css:'prod/css/',
+        img:'prod/img/',
+    }
+};
+
+gulp.task('test', ()=>{return console.log('gulp is running')})
+
+// *******************DEV**************//
+gulp.task('sass', ()=>{
+    gulp.src(path.src.scss)
+    .pipe(sass())
+    .pipe(gulp.dest(path.src.css))
+    .pipe(browserSync.stream());
+})
+
+
+gulp.task('serve', ['test', 'sass'], () =>{
+    browserSync.init({
+        server:'./src/'
+    });
+    gulp.watch([path.src.scss], ['sass', browserSync.reload ]);
+    gulp.watch([path.src.html]).on('change', browserSync.reload);
+    gulp.watch([path.src.js]).on('change', browserSync.reload);
+    gulp.watch([path.src.img]).on('change', browserSync.reload);
+    gulp.watch(['./src/css/*.css'],['dangercss', browserSync.reload ]);
+    
+})
+
+gulp.task('default', ['serve'])
+
+
+
+// *******************BUILD**************//
+gulp.task('htmlBuild', ()=>{
+    gulp.src(path.src.html)
+    .pipe(gulp.dest(path.build.html))
+})
+gulp.task('jsBuild', () => {
+    gulp.src([path.src.js])
+    .pipe(gulp.dest(path.build.js))
+})
+gulp.task('cssBuild', ()=>{
+    gulp.src(path.src.css.concat('*.css'))
+    .pipe(gulp.dest(path.build.css))
+})
+gulp.task('imgBuild', ()=>{
+    gulp.src(path.src.img)
+    .pipe(imagemin())
+    .pipe(gulp.src(path.build.img))
+})
+gulp.task('build', ['htmlBuild', 'jsBuild', 'cssBuild', 'imgBuild'])
+
+gulp.task('serveBuild',['build'], ()=>{
+    browserSync.init({
+        server:'./prod/',
+        port: 3000,
+    });
+})
