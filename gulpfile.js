40dd9f0538e60d809ab75897a02ac338427ade03
hyaovi <hfyaovi@gmail.com>
2019-12-04 01:50:37 +0300

    initial commit based on gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,81 +1,84 @@
-const
-    browserSync = require('browser-sync').create(),
-    gulp =  require('gulp'),
-    sass = require('gulp-sass'),
-    imagemin = require('gulp-imagemin'),
-    autoprefixer = require('gulp-autoprefixer')
-;
+const { src, dest, parallel, series, watch } = require('gulp');
+const browserSync = require('browser-sync').create();
+const gulpSass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+const autoprefixer = require('gulp-autoprefixer');
 
 const path = {
-    src:{
-        html:'src/*.html',
-        scss:'src/scss/*.scss',
-        js:'src/js/*.js',
-        css:'src/css/',
-        img:'src/img/*',
-    },
-    build:{
-        html:'prod/',
-        js:'prod/js/',
-        css:'prod/css/',
-        img:'prod/img/',
-    }
+  src: {
+    html: 'src/*.html',
+    scss: 'src/scss/*.scss',
+    js: 'src/js/*.js',
+    css: 'src/css/',
+    img: 'src/img/*'
+  },
+  build: {
+    html: 'build/',
+    js: 'build/js/',
+    css: 'build/css/',
+    img: 'build/img/'
+  }
 };
 
 // *******************DEV**************//
-gulp.task('sass', ()=>{
-    gulp.src(path.src.scss)
-    .pipe(sass())
-    .pipe(autoprefixer({
-        browsers:['last 20 versions'],
-        cascade: false,
-    }))
-    .pipe(gulp.dest(path.src.css))
+const sass = () => {
+  return src(path.src.scss)
+    .pipe(gulpSass())
+    .pipe(
+      autoprefixer({
+        browsers: ['last 20 versions'],
+        cascade: false
+      })
+    )
+    .pipe(dest(path.src.css))
     .pipe(browserSync.stream());
-})
+};
 //import bootstrap js and dependencies
-gulp.task('bootstrap', () => {
-    gulp.src(['./node_modules/jquery/dist/jquery.js', './node_modules/popper.js/dist/popper.js','./node_modules/bootstrap/dist/js/bootstrap.js'])
-    .pipe(gulp.dest('./src/js'))
-})
-gulp.task('serve', [ 'sass','bootstrap'], function(){
-    browserSync.init({
-        server:'./src/'
-    });
-    gulp.watch([path.src.scss], ['sass' ]);
-    gulp.watch([path.src.html]).on('change', browserSync.reload);
-    gulp.watch([path.src.js]).on('change', browserSync.reload);
-    gulp.watch([path.src.img]).on('change', browserSync.reload);
-    
-})
+const bootstrap = () => {
+  return src([
+    './node_modules/jquery/dist/jquery.js',
+    './node_modules/popper.js/dist/popper.js',
+    './node_modules/bootstrap/dist/js/bootstrap.js'
+  ]).pipe(dest('./src/js'));
+};
+const launchBrowser = (path = './src/', port = 3000) => {
+  browserSync.init({
+    server: path,
+    port
+  });
+};
 
-gulp.task('default', ['serve'])
+const watchSass = () => watch([path.src.scss], sass);
+const watchHtml = () => watch([path.src.html], browserSync.reload);
+const watchJs = () => watch([path.src.js], browserSync.reload);
+const watchImg = () => watch([path.src.img], browserSync.reload);
 
+const serve = series(parallel(sass, bootstrap), function() {
+  launchBrowser();
+  watchSass();
+  watchHtml();
+  watchJs();
+  watchImg();
+});
 
+exports.default = serve;
 
 // *******************BUILD**************//
-gulp.task('htmlBuild', ()=>{
-    gulp.src(path.src.html)
-    .pipe(gulp.dest(path.build.html))
-})
-gulp.task('jsBuild', () => {
-    gulp.src([path.src.js])
-    .pipe(gulp.dest(path.build.js))
-})
-gulp.task('cssBuild', ()=>{
-    gulp.src(path.src.css.concat('*.css'))
-    .pipe(gulp.dest(path.build.css))
-})
-gulp.task('imgBuild', ()=>{
-    gulp.src(path.src.img)
+const htmlBuild = () => {
+  return src(path.src.html).pipe(dest(path.build.html));
+};
+const jsBuild = () => {
+  return src([path.src.js]).pipe(dest(path.build.js));
+};
+const cssBuild = () => {
+  return src(path.src.css.concat('*.css')).pipe(dest(path.build.css));
+};
+const imgBuild = () => {
+  return src(path.src.img)
     .pipe(imagemin())
-    .pipe(gulp.dest(path.build.img))
-})
-gulp.task('build', ['htmlBuild', 'jsBuild', 'cssBuild', 'imgBuild'])
-
-gulp.task('serveBuild',['build'], ()=>{
-    browserSync.init({
-        server:'./prod/',
-        port: 3000,
-    });
-})
+    .pipe(dest(path.build.img));
+};
+const build = parallel(htmlBuild, jsBuild, cssBuild, imgBuild);
+const serveBuild = series(build, () => launchBrowser('./prod/', 8000));
+exports.build = build;
+exports.serveBuild = serveBuild;
